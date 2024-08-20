a9c7e641a21cad58dc953b3ff3fa6eb0f32df63e
jacob_timmerman <jaketimm.01@gmail.com>
2024-08-20 12:55:53 -0400

    blog update

--- blog_API_scripts.js
+++ blog_API_scripts.js
@@ -1,15 +1,17 @@
-//////////////////////////////////////////////////
 // Word Press API Blog Integration
 // Replace yourwebsite.com only
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const apiUrl = 'https://public-api.wordpress.com/wp/v2/sites/webdevtestblog5.wordpress.com/posts';
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
             const postsContainer = document.getElementById('posts-container');
-            data.forEach(post => {
+            // Limit the number of posts to 6
+            const postsToShow = data.slice(0, 6);
+
+            postsToShow.forEach(post => {
                 const postElement = document.createElement('div');
                 postElement.classList.add('post');
 
@@ -26,17 +28,15 @@ document.addEventListener('DOMContentLoaded', function() {
                 const truncatedExcerpt = truncateText(post.excerpt.rendered, 50);
 
                 postElement.innerHTML = `
-                    <h2><a target="_blank">${post.title.rendered}</a></h2>
+                    <h2><a target="_blank" href="${post.link}">${post.title.rendered}</a></h2>
                     <div>${truncatedExcerpt}</div>
                     <div class="read-more-container">
                       <a class="post-button-solid" aria-label="Read more about ${post.title.rendered}" href="${post.link}">Read More</a>
                     </div>
-`;
-
+                `;
 
                 postsContainer.appendChild(postElement);
             });
         })
         .catch(error => console.error('Error fetching posts:', error));
 });
-

efec7270a61a8b4518cbd6e4d086c3deb6ff7d40
jacob_timmerman <jaketimm.01@gmail.com>
2024-09-03 19:07:32 -0400

    blog

---
+++ generate_blog_files.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+
+const blogFolder = path.join(__dirname, 'blog');
+const outputFilePath = path.join(__dirname, 'blogFiles.json');
+
+// Read all Markdown files from the blog folder
+fs.readdir(blogFolder, (err, files) => {
+    if (err) {
+        console.error('Error reading blog folder:', err);
+        return;
+    }
+
+    const markdownFiles = files.filter(file => path.extname(file) === '.md');
+
+    // Write the list of Markdown files to a JSON file
+    fs.writeFile(outputFilePath, JSON.stringify(markdownFiles), err => {
+        if (err) {
+            console.error('Error writing blogFiles.json:', err);
+        } else {
+            console.log('blogFiles.json has been created.');
+        }
+    });
+});
